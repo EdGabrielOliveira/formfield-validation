51a9b53853b37dd22cd7cf3bf13d4a20424ac297
EdGabrielOliveira <edgabrieloliveira@proton.me>
2024-11-18 09:30:32 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
-import Image from "next/image";
+"use client";
+
+import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+
+export const useValidation = () => {
+  return Yup.object().shape({
+    fullName: Yup.string().required("Nome completo é obrigatório!"),
+    email: Yup.string().email("Formato inválido").required("E-mail é obrigatório!"),
+    password: Yup.string().required("Senha é obrigatório!"),
+    confirmPass: Yup.string()
+      .required("Senha é obrigatório!")
+      .oneOf([Yup.ref("password")], "Senha não coincide"),
+  });
+};
+
+type InputsValidation = {
+  fullName: string;
+  email: string;
+  password: string;
+  confirmPass: string;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputsValidation>({ resolver: yupResolver(useValidation()) });
+  const onSubmit: SubmitHandler<InputsValidation> = (data) => console.log(data);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <main className="container-main">
+      <div className="flex flex-col items-center justify-center h-screen gap-12">
+        <div className="text-center text-2xl font-semibold">
+          <h1> Validação de Formulário </h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
+          <div className="case-form">
+            <label>Nome completo</label>
+            <input className="input-form" type="text" {...register("fullName")} />
+            <span className="error-form">{errors.fullName?.message}</span>
+          </div>
+          <div className="case-form">
+            <label>E-mail</label>
+            <input className="input-form" type="email" {...register("email")} />
+            <span className="error-form">{errors.email?.message}</span>
+          </div>
+          <div className="case-form">
+            <label>Senha</label>
+            <input className="input-form" type="password" {...register("password")} />
+            <span className="error-form">{errors.password?.message}</span>
+          </div>
+          <div className="case-form">
+            <label>Confirmar senha</label>
+            <input className="input-form" type="password" {...register("confirmPass")} />
+            <span className="error-form">{errors.confirmPass?.message}</span>
+          </div>
+          <button className="button-form" type="submit">
+            Registrar
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
